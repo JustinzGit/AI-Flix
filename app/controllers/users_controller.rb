094fdff7222bdf6c6507b6299b3f15c97547c6c1
Justin <justinms@outlook.com>
2020-06-04 15:07:42 -0500

    assign session user id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   # Sign Up
   def create
     @user = User.create(user_params)
+    session[:user_id] = @user.id
     redirect_to users_path
   end
 
