5c8aaa4beaf99a7788298306bda3874fdf5c6223
Justin <justinms@outlook.com>
2020-06-04 15:14:57 -0500

    add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # User Controller 
   resources :users, only: [:index, :new, :create]
   get '/homepage', to: 'users#homepage'
+
+  # Sessions Controller
+  get '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
 end
