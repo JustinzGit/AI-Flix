290cbd6457cc61a91e16d5e10c08e3117c864067
Justin <justinms@outlook.com>
2020-06-04 09:50:11 -0500

    seed db with small data set

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,18 @@
 
 require 'csv'
 
-actors = CSV.parse(File.read("actor.csv"), headers: true)
+actors = CSV.parse(File.read("small/actors.csv"), headers: true)
+movies = CSV.parse(File.read("small/movies.csv"), headers: true)
+movie_actors = CSV.parse(File.read("small/movie_actors.csv"), headers: true)
 
 actors.each do |a|
-  p a["name"]
+  Actor.create(IMBD_ID: a["id"].to_i, name: a["name"], birth: a["birth"])
+end
+
+movies.each do |m|
+  Movie.create(IMBD_ID: m["id"].to_i, title: m["title"], year: m["title"])
+end
+
+movie_actors.each do |ma|
+  MovieActor.create(actor_id: ma["person_id"].to_i, movie_id: ma["movie_id"].to_i)
 end
