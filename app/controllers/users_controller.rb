fe9fc5ea16e5898af145916bddaf7e3da937c640
Justin <justinms@outlook.com>
2020-06-04 13:29:40 -0500

    add index and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to users_path
   end
 
   private
