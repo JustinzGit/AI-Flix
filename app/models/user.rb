a3459aaafd5677c6ff8c6765c39670d94c90ac5b
Justin <justinms@outlook.com>
2020-06-04 13:29:02 -0500

    add password macro

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true, confirmation: true
   validates :password_confirmation, presence: true
+
+  has_secure_password
 end
