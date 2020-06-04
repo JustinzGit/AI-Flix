f9c1a1e123a41715a76ca08abd852a89f86d6cb5
Justin <justinms@outlook.com>
2020-06-04 15:34:28 -0500

    add create action, sign user in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
-end 
+
+  def login
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to homepage_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def logout
+  end
+end
