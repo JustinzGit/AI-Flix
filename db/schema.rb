ff845f6cb7901bad8f0c4ddba19cd4ccace60b82
Justin <justinms@outlook.com>
2020-06-04 14:23:15 -0500

    add username to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_135828) do
+ActiveRecord::Schema.define(version: 2020_06_04_192208) do
 
   create_table "actors", force: :cascade do |t|
     t.integer "IMBD_ID"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_135828) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
 end
