42e70d6d3a21fe3201d6e685a00736411baa159e
Justin <justinms@outlook.com>
2020-06-05 16:01:51 -0500

    add queue fronteir methods

--- app/models/concerns/ai_utilities.rb
+++ app/models/concerns/ai_utilities.rb
@@ -10,4 +10,29 @@ module AiUtilities
     end
   end
 
+  class QueueFronteir
+    @@fronteir = []
+
+    def add(node)
+      @@fronteir << node
+    end
+
+    def contains_state(state)
+      @@fronteir.any? {|node| node.state == state}
+    end
+
+    def empty?
+      @@fronteir.empty?
+    end
+
+    def remove
+      if self.empty?
+        raise IndexError, "Empty Fronteir"
+      else
+        node = @@fronteir.shift
+        return node
+      end
+    end
+
+  end
 end
