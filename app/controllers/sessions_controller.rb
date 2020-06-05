687144bfc65f275e1d062ff80f2482c60120b3f2
Justin <justinms@outlook.com>
2020-06-05 10:57:51 -0500

    add homepage action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
 
+  # GET Login
   def new
     @user = User.new
   end
 
+  # POST Login
   def create
     @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
@@ -14,6 +16,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def homepage
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  # GET Logout
   def destroy
     session.clear
     redirect_to login_path
