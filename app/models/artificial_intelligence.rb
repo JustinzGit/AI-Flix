c644157e7eb7854d7644a0b05207fbf6a1eaeae9
Justin <justinms@outlook.com>
2020-06-06 11:04:19 -0500

    add actor neighbors method

--- app/models/artificial_intelligence.rb
+++ app/models/artificial_intelligence.rb
@@ -1,3 +1,16 @@
 class ArtificialIntelligence < ApplicationRecord
+  include AiUtilities
+  require 'set'
 
+  def neighbors_for_actor(actor_id)
+    actor = Actor.find(actor_id)
+    neighbors = Set.new
+
+    actor.movies.each do |movie|
+      movie.actors.each do |actor|
+        neighbors.add(actor)
+      end
+    end
+    neighbors
+  end
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,7 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :actors
+  has_many :movies
+
+  accepts_nested_attributes_for :actors
 end
