ab87572c35145b20db95bb944382ec51eff72553
Justin <justinms@outlook.com>
2020-06-07 16:25:19 -0500

    add notes

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -4,9 +4,27 @@ class PlaylistsController < ApplicationController
   end
 
   def create
-    render plain: params.inspect
-    # actor_1 = params[:actor_1]
-    # actor_2 = params[:actor_2]
-    # @ai_results = ArtificialIntelligence.shortest_path
+    # Acquire actor names from playlist form
+    actor_1 = Actor.find(params[:actor_1])
+    actor_2 = Actor.find(params[:actor_2])
+
+    # Name playlist using actor names
+    name = "#{actor_1.name} and #{actor_2.name} Playlist"
+
+    # Generate playlist of movies that link the two actors
+    path = Search.shortest_path(actor_1, actor_2)
+
+    @playlist = Playlist.new(name: name, user_id: session[:user_id])
+
+    # Store results from algorithm to playlist
+    @playlist.movies << path[:movies]
+    @playlist.actors << path[:actors]
+    @playlist.save
+
+    redirect_to playlist_path(@playlist)
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
   end
 end
