bfbf1cbb1267142a6ba8775e54a4b93e4aceb1b1
Justin <justinms@outlook.com>
2020-06-07 15:41:52 -0500

    remove associations with playlist

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,6 @@
 class Movie < ApplicationRecord
   has_many :movie_actors
   has_many :actors, through: :movie_actors
-
-  has_many :playlist_movies
-  has_many :playlists, through: :playlist_movies
-
-
+  
   self.primary_key = "IMBD_ID"
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,9 +1,3 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-
-  has_many :playlist_actors
-  has_many :playlist_movies
-  
-  has_many :actors, through: :playlist_actors
-  has_many :movies, through: :playlist_movies
 end
