bafa39886c729ee4d18d9a0e30970eae9e9b7b5c
Justin <justinms@outlook.com>
2020-06-07 09:26:01 -0500

    renamed module to search utilities

---
+++ app/models/concerns/search_utilities.rb
@@ -0,0 +1,38 @@
+module SearchUtilities
+
+  class Node
+    attr_accessor :state, :parent, :action
+    def initialize(state, parent, action)
+      @state = state
+      @parent = parent
+      @action = action
+    end
+  end
+
+  class QueueFronteir
+    def initialize()
+      @fronteir = []
+    end
+
+    def add(node)
+      @fronteir << node
+    end
+
+    def contains_state(state)
+      @fronteir.any? {|node| node.state == state}
+    end
+
+    def empty?
+      @fronteir.empty?
+    end
+
+    def remove
+      if self.empty?
+        raise IndexError, "Empty Fronteir"
+      else
+        node = @fronteir.shift
+        return node
+      end
+    end
+  end
+end
