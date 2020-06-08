2a369a8086dc4c8c09d25b49dd82a2f06b2a22c9
Justin <justinms@outlook.com>
2020-06-08 09:46:57 -0500

    add helper to output path

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,12 @@
 module PlaylistsHelper
+  def organize_path(playlist)
+    movies = playlist.movies
+    actors = playlist.actors
+    string = ""
+
+    movies.length.times do |i|
+      string += "#{actors[i].name} and #{actors[i + 1].name} starred in #{movies[i].title}<br>"
+    end
+    string.html_safe
+  end
 end
