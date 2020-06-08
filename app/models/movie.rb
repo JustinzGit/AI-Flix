033725a43e571cb655e8e8b560cb702de8c6c2b5
Justin <justinms@outlook.com>
2020-06-08 17:16:14 -0500

    fetch movie details from tmdb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,10 @@ class Movie < ApplicationRecord
     response = JSON.parse response.body
     response['results'].empty? ? nil : response['results'][0]['id']
   end
+
+  def self.get_movie_details(movie_id)
+    api_key = ENV['tmdb_api_key']
+    response = Faraday.get "https://api.themoviedb.org/3/movie/#{movie_id}?api_key=#{api_key}"
+    response = JSON.parse response.body
+  end
 end
