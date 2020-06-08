2f68258e0ed21800bb19ab690e55065ea86298d5
Justin <justinms@outlook.com>
2020-06-08 12:55:39 -0500

    add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
 
+  def current_user
+    User.find(session[:user_id])
+  end
+
   def require_login
     if session[:user_id].nil?
       flash[:alert] = "You must be logged in."
       redirect_to login_path
-    end 
+    end
   end
 end
