5742daeae7bdced72cee0fc24d839374569dbb91
Justin <justinms@outlook.com>
2020-06-09 09:02:41 -0500

    methods to grab actor data from tmdb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,5 +4,32 @@ class Actor < ApplicationRecord
 
   has_many :playlist_actors
   has_many :playlists, through: :playlist_actors
+
   self.primary_key = "IMBD_ID"
+
+  def self.get_actor_id(actor)
+    api_key = ENV['tmdb_api_key']
+    response = Faraday.get "https://api.themoviedb.org/3/search/person?api_key=#{api_key}&language=en-US&query=#{actor.name}"
+    response = JSON.parse response.body
+    response['results'].empty? ? nil : response['results'][0]['id']
+  end
+
+  def self.get_actor_data(actor_id)
+    api_key = ENV['tmdb_api_key']
+    response = Faraday.get "https://api.themoviedb.org/3/person/#{actor_id}?api_key=#{api_key}&language=en-US"
+    response = JSON.parse response.body
+    actor_data = {
+      birthday: response['birthday'],
+      deathday: response['deathday'],
+      biography: response['biography'],
+      brithplace: response['place_of_birth'],
+      image: response['profile_path'],
+      imdb_id: response['imdb_id']
+    }
+  end
+
+  def self.get_actor(actor)
+    actor_id = self.get_actor_id(actor)
+    self.get_actor_data(actor_id)
+  end
 end
