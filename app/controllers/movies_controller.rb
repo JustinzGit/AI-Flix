afd1a0e3ea081d0fa269d620ab1fa1651ba7656b
Justin <justinms@outlook.com>
2020-06-09 10:25:46 -0500

    add a movies actor collection to show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,6 +5,7 @@ class MoviesController < ApplicationController
 
   def show
     @movie = Movie.find(params[:id])
+    @actors = @movie.actors
     @movie = Movie.get_movie(@movie.title)
   end
 end
