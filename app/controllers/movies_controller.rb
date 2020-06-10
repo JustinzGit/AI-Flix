4f85e9ff8b9402029101aec365545a7354f83242
Justin <justinms@outlook.com>
2020-06-10 16:22:52 -0500

    add index conditons to collect movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,14 @@
 class MoviesController < ApplicationController
+
   def index
-    @movies = Movie.all.order('title')
+    if params[:search] && !params[:search].empty?
+      @movies = Movie.begins_with(params[:search]).paginate(page: params[:page], per_page: 20)
+    end
+
+    if params[:date] && !params[:date][:year].empty?
+      @movies = Movie.released_in(params[:date][:year]).paginate(page: params[:page], per_page: 20)
+    end
+
   end
 
   def show
