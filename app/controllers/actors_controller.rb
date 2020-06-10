1934229d969ba9dc23d66bd953a1f3a1b9568be7
Justin <justinms@outlook.com>
2020-06-10 16:48:01 -0500

    display actors via search params

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,13 @@
 class ActorsController < ApplicationController
 
   def index
-    @actors = Actor.all.order('name')
+    if params[:search] && !params[:search].empty?
+      @actors = Actor.find_by_name(params[:search]).paginate(page: params[:page], per_page: 20)
+    end
+
+    if params[:date] && !params[:date][:year].empty?
+      @actors = Actor.find_by_birth_year(params[:date][:year]).paginate(page: params[:page], per_page: 20)
+    end
   end
 
   def show
