da09516332265b495f56c200e664a55143cdbc9c
Justin <justinms@outlook.com>
2020-06-11 08:40:34 -0500

    allow user to select and remove actors for playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,12 +1,42 @@
 class PlaylistsController < ApplicationController
 
   def new
+    actor_name = params[:search] if params[:search] && !params[:search].blank?
+    birth_year = params[:date][:year] if params[:date] && !params[:date][:year].blank?
+
+    if actor_name && birth_year
+      @actors = Actor.find_actors(actor_name, birth_year)
+
+    elsif actor_name
+      @actors = Actor.find_by_name(actor_name)
+
+    elsif birth_year
+      @actors = Actor.find_by_birth_year(params[:date][:year])
+    end
+
+    @actors = @actors.paginate(page: params[:page], per_page: 20) if @actors
+  end
+
+  def actor_1
+    session[:actor_1] ||= Actor.find(params[:actor_1])
+    redirect_to new_playlist_path
+  end
+
+  def actor_2
+    session[:actor_2] ||= Actor.find(params[:actor_2])
+    redirect_to new_playlist_path
+  end
+
+  def clear_actors
+    session[:actor_1] = nil
+    session[:actor_2] = nil
+    redirect_to new_playlist_path
   end
 
   def create
     # Acquire actor names from playlist form
-    actor_1 = Actor.find(params[:actor_1])
-    actor_2 = Actor.find(params[:actor_2])
+    actor_1 = Actor.find(session[:actor_1])
+    actor_2 = Actor.find(session[:actor_2])
 
     # Name playlist using actor names
     name = "#{actor_1.name} and #{actor_2.name} Playlist"
