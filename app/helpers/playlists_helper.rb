2498a93b10028fd536ec4b3e50b21c51d52a65a7
Justin <justinms@outlook.com>
2020-06-11 08:41:16 -0500

    methods to reveal actor names and links

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,4 +1,5 @@
 module PlaylistsHelper
+
   def organize_path(playlist)
     movies = playlist.movies
     actors = playlist.actors
@@ -8,5 +9,21 @@ module PlaylistsHelper
       string += "#{actors[i].name} and #{actors[i + 1].name} starred in #{movies[i].title}<br>"
     end
     string.html_safe
+  end
+
+  def actor_1_name
+    session[:actor_1]['name'] if session[:actor_1]
+  end
+
+  def actor_2_name
+    session[:actor_2]['name'] if session[:actor_2]
+  end
+
+  def playlist_actor_link(actor)
+    if actor_1_name.nil?
+      playlist_actor_1_path(actor)
+    else
+      playlist_actor_2_path(actor)
+    end
   end
 end
