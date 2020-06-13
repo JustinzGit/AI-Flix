a63d48ca93530eea636cfb7c39a64c9cb70c22f5
Justin <justinms@outlook.com>
2020-06-13 14:54:47 -0500

    setup playlist create action

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -2,4 +2,15 @@ class PlaylistsController < ApplicationController
   def new
     @playlist = Playlist.new
   end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+    redirect_to playlists_path
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name)
+  end
 end
