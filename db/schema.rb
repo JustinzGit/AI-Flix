26246bf59b880d4a633bfde361de78c6070841eb
Justin <justinms@outlook.com>
2020-06-13 09:57:26 -0500

    create reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_205500) do
+ActiveRecord::Schema.define(version: 2020_06_13_145214) do
 
   create_table "actors", force: :cascade do |t|
     t.integer "IMBD_ID"
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2020_06_07_205500) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "user_id"
+    t.string "user_review"
   end
 
   create_table "users", force: :cascade do |t|
