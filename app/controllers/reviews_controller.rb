210e8f9d69061e96bce9d5ec623a4819fc27a4ed
Justin <justinms@outlook.com>
2020-06-13 10:28:36 -0500

    add ability to create a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,14 @@
 class ReviewsController < ApplicationController
-  
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+    redirect_to movie_path(@review.movie_id)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_review, :user_id, :movie_id)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :movie 
+  belongs_to :movie
+
+  validates :user_review, presence: true 
 end
