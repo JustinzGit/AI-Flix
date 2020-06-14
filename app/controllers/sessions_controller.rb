cdc3970ebd3c2b49464b6abcc52f4e9137cff9a8
Justin <justinms@outlook.com>
2020-06-14 10:43:09 -0500

    log user in via github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,19 @@ class SessionsController < ApplicationController
 
   # POST Login
   def create
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      redirect_to homepage_path
+    if !auth_hash[:uid].nil?
+      @user = User.find_or_create_by(uid: auth_hash[:uid]) do |u|
+        u.username = auth_hash[:info][:nickname]
+        u.email = auth_hash[:info][:email]
+      end
     else
-      redirect_to login_path
+      @user = User.find_by(username: params[:user][:username])
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to homepage_path
+      else
+        redirect_to login_path
+      end
     end
   end
 
@@ -30,5 +37,9 @@ class SessionsController < ApplicationController
 
   def redirect_home_if_logged_in
     redirect_to homepage_path if !session[:user_id].nil?
+  end
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
