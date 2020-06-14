ccfcc20fd1d6d6729d749bb4707ad79859e69e9e
Justin <justinms@outlook.com>
2020-06-14 11:22:39 -0500

    add uid to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_201131) do
+ActiveRecord::Schema.define(version: 2020_06_14_154348) do
 
   create_table "actors", force: :cascade do |t|
     t.integer "IMBD_ID"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_06_13_201131) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.integer "uid"
   end
 
 end
