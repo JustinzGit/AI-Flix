84a080eb23921d6ecbf911eb1ae00cac0cea4621
Justin <justinms@outlook.com>
2020-06-15 12:34:58 -0500

    remove signup with auth hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,23 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login
+
   before_action :redirect_home_if_logged_in
   skip_before_action :redirect_home_if_logged_in, only: [:destroy]
 
-  skip_before_action :require_login
-
-  # GET Login
   def new
     @user = User.new
   end
 
-  # POST Login
   def create
-    if auth_hash["uid"]
-      @user = User.find_or_create_by(uid: auth_hash["uid"]) do |u|
-        u.username = auth_hash["info"]["nickname"]
-        u.email = auth_hash["info"]["email"]
-        u.password = auth_hash[:credentials][:token]
-      end
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to homepage_path
     else
-      @user = User.find_by(username: params[:user][:username])
-      if @user && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        redirect_to homepage_path
-      else
-        redirect_to login_path
-      end
+      redirect_to login_path
     end
   end
 
-  # GET Logout
   def destroy
     session.clear
     redirect_to login_path
@@ -40,9 +27,5 @@ class SessionsController < ApplicationController
 
   def redirect_home_if_logged_in
     redirect_to homepage_path if !session[:user_id].nil?
-  end
-
-  def auth_hash
-    request.env['omniauth.auth']
   end
 end
