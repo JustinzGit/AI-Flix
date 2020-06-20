ab2452ae1c4631590ad667cfddc4bb3987c1c9d9
Justin <justinms@outlook.com>
2020-06-20 14:39:46 -0500

    add actor_birthplace helper method

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -7,4 +7,8 @@ module ActorsHelper
     def actor_birthday(actor)
         actor[:birthday].nil? ? "No Data" : format_date(actor[:birthday])
     end 
+
+    def actor_birthplace(actor)
+        actor[:birthplace].nil? ? "No Data" : actor[:birthplace]
+    end 
 end
