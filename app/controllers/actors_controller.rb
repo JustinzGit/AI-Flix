eefa05d3c1cfe5d1e9a1db0be225c69845199c30
Justin <justinms@outlook.com>
2020-06-20 14:07:38 -0500

    change param key

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,11 @@
 class ActorsController < ApplicationController
 
   def search
-    name = params[:search] if params[:search] && !params[:search].blank?
+    name = params[:name] if params[:name] && !params[:name].blank?
     birth_year = params[:date][:year] if params[:date] && !params[:date][:year].blank?
 
     if name
-      @actors = Actor.find_by_name(params[:search])
+      @actors = Actor.find_by_name(params[:name])
     elsif birth_year
       @actors = Actor.find_by_birth_year(params[:date][:year])
     end
