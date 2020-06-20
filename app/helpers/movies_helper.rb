1aa27e527a0d7ad4907e408abdc50f4ee8430baf
Justin <justinms@outlook.com>
2020-06-20 16:15:04 -0500

    add movie_budget helper method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,5 +1,9 @@
 module MoviesHelper
     def movie_date(movie)
         format_date(movie[:release_date])
+    end
+    
+    def movie_budget(movie)
+        number_to_currency(movie[:budget])
     end 
 end
