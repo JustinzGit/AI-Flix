396fab2b11b174293a6bde7e356e5ca361d820ce
Justin <justinms@outlook.com>
2020-06-20 14:38:48 -0500

    add actor_birthday helper method

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -2,5 +2,9 @@ module ActorsHelper
     def actor_link(name, birth)
         birth = "N/A" if birth.nil?
         "#{name} - Born: #{birth}"
+    end
+    
+    def actor_birthday(actor)
+        actor[:birthday].nil? ? "No Data" : format_date(actor[:birthday])
     end 
 end
