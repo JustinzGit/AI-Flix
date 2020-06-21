8d72dadfa142c7d6fb5432223f4e03dec54dada3
Justin <justinms@outlook.com>
2020-06-21 12:08:35 -0500

    return nil if actor doesn't exist in tmdb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -17,18 +17,22 @@ class Actor < ApplicationRecord
 
   # Use actor ID to obtain actor data from TMDB
   def self.get_actor_data(actor_id)
-    api_key = ENV['tmdb_api_key']
-    response = Faraday.get "https://api.themoviedb.org/3/person/#{actor_id}?api_key=#{api_key}&language=en-US"
-    response = JSON.parse response.body
-    actor_data = {
-      name: response['name'],
-      birthday: response['birthday'],
-      deathday: response['deathday'],
-      biography: response['biography'],
-      birthplace: response['place_of_birth'],
-      image: response['profile_path'],
-      imdb_id: response['imdb_id']
-    }
+    if actor_id.nil?
+      return nil
+    else 
+      api_key = ENV['tmdb_api_key']
+      response = Faraday.get "https://api.themoviedb.org/3/person/#{actor_id}?api_key=#{api_key}&language=en-US"
+      response = JSON.parse response.body
+      actor_data = {
+        name: response['name'],
+        birthday: response['birthday'],
+        deathday: response['deathday'],
+        biography: response['biography'],
+        birthplace: response['place_of_birth'],
+        image: response['profile_path'],
+        imdb_id: response['imdb_id']
+      }
+    end 
   end
 
   # Obtain TMDB data on provided actor 
