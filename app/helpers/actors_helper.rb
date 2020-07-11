6bd4cbbb9077ad00c2b2a28ed77c1cda11e7a50f
Justin <justinms@outlook.com>
2020-07-11 10:52:00 -0500

    remove link helper

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -1,9 +1,4 @@
 module ActorsHelper
-    def actor_link(name, birth)
-        birth = "N/A" if birth.nil?
-        "#{name} - Born: #{birth}"
-    end
-    
     def actor_birthday(actor)
         actor[:birthday].nil? ? "No Data" : format_date(actor[:birthday])
     end 
