4a1d4c1086aa99abd35e73da0251380a783a24cf
Justin <justinms@outlook.com>
2020-07-11 10:57:46 -0500

    remove search by year

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,9 @@ class MoviesController < ApplicationController
 
   def search
     title = params[:title] if params[:title] && !params[:title].blank?
-    year = params[:date][:year] if params[:date] && !params[:date][:year].blank?
 
-    if title && year
-      @movies = Movie.find_movies(title, year)
-    elsif title
+    if title
       @movies = Movie.begins_with(title)
-    elsif year
-      @movies = Movie.released_in(year)
     end
 
     @movies = @movies.paginate(page: params[:page], per_page: 20) if @movies
