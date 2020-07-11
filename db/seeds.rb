c31c75ab377d1afe62dbb9ebe36db6bce051ac1a
Justin <justinms@outlook.com>
2020-07-11 10:34:03 -0500

    use alternative database

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,21 @@
 
 require 'csv'
 
-actors = CSV.parse(File.read("large/actors.csv"))
+actors = CSV.parse(File.read("tmdb_dataset/actors.csv"))
 columns = actors.shift
-columns[0] = "IMBD_ID"
+columns[0] = "tmdb_id"
 columns.map! &:to_sym
 Actor.import columns, actors
 
-movies = CSV.parse(File.read("large/movies.csv"))
+movies = CSV.parse(File.read("tmdb_dataset/movies.csv"))
 columns = movies.shift
-columns[0] = "IMBD_ID"
+columns[0] = "tmdb_id"
 columns.map! &:to_sym
 Movie.import columns, movies
 
-movie_actors = CSV.parse(File.read("large/movie_actors.csv"))
+movie_actors = CSV.parse(File.read("tmdb_dataset/movie_actors.csv"))
 columns = movie_actors.shift
-columns[0] = "actor_id"
-columns[1] = "movie_id"
+columns[1] = "actor_id"
+columns[0] = "movie_id"
 columns.map! &:to_sym
 MovieActor.import columns, movie_actors
