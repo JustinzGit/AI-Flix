55a2a1957ae74f61973ef240f687aabc383be8f1
Justin <justinms@outlook.com>
2021-05-03 16:02:12 -0500

    change param name

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,10 +2,10 @@ class ActorsController < ApplicationController
 
   # GET /actors/search
   def search
-    @actors = Actor.search_by_name(params[:name])
+    @actors = Actor.search_by_name(params[:query])
   end
 
-  # GET /actors/:id
+  # GET /actors/:id (tmdb id)
   def show
     @actor = Actor.find(params[:id])
     @actor.fetch_tmdb_data
