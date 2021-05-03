66d70def92c74cb27098045168dd7f4c23024fd0
Justin <justinms@outlook.com>
2021-05-03 10:00:08 -0500

    add search results helper

--- app/helpers/actors_helper.rb
+++ app/helpers/actors_helper.rb
@@ -10,4 +10,14 @@ module ActorsHelper
     def actor_deathday(actor)
         actor[:deathday].nil? ? "Still Living" : format_date(actor[:deathday])
     end 
+
+    def display_actors_search_results(results)
+        if results
+            count = "<h3 class='list-title'>We found #{results.count} Actors</h3>"
+            actors = results.map do |a|
+                "<p>#{link_to(a.name, actor_path(a))}</p>"
+            end
+            raw(count + actors.join) 
+        end
+    end 
 end
