2e4af22a01cf31593c1b1f804e84a0d6112f8184
Justin <justinms@outlook.com>
2021-05-03 15:44:20 -0500

    refactor methods for obtaining tmdb data

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,41 +7,35 @@ class Actor < ApplicationRecord
 
   self.primary_key = "tmdb_id"
 
-  # Search TMDB for actor ID
-  def self.get_actor_id(actor)
-    api_key = ENV['tmdb_api_key']
-    response = Faraday.get "https://api.themoviedb.org/3/search/person?api_key=#{api_key}&language=en-US&query=#{actor}"
-    response = JSON.parse response.body
-    response['results'].empty? ? nil : response['results'][0]['id']
+  # obtain TMDB actor id
+  # def get_actor_id(actor)
+  #   api_key = ENV['tmdb_api_key']
+  #   response = Faraday.get "https://api.themoviedb.org/3/search/person?api_key=#{api_key}&language=en-US&query=#{actor}"
+  #   response = JSON.parse response.body
+  #   response['results'][0]['id'] if !response['results'].empty?
+  # end
+
+  # fetch data from TMDB
+  # if data has not been collected
+  def fetch_tmdb_data
+      if !self.data_collected
+        api_key = ENV['tmdb_api_key']
+        response = Faraday.get "https://api.themoviedb.org/3/person/#{self.id}?api_key=#{api_key}&language=en-US"
+        response = JSON.parse response.body
+        self.birthday = response['birthday']
+        self.deathday = response['deathday']
+        self.biography = response['biography']
+        self.birthplace = response['place_of_birth']
+        self.image = response['profile_path']
+        self.imdb_id = response['imdb_id']
+        self.data_collected = true
+
+        # save data to db
+        self.save
+      end 
   end
 
-  # Use actor ID to obtain actor data from TMDB
-  def self.get_actor_data(actor_id)
-    if actor_id.nil?
-      return nil
-    else 
-      api_key = ENV['tmdb_api_key']
-      response = Faraday.get "https://api.themoviedb.org/3/person/#{actor_id}?api_key=#{api_key}&language=en-US"
-      response = JSON.parse response.body
-      actor_data = {
-        name: response['name'],
-        birthday: response['birthday'],
-        deathday: response['deathday'],
-        biography: response['biography'],
-        birthplace: response['place_of_birth'],
-        image: response['profile_path'],
-        imdb_id: response['imdb_id']
-      }
-    end 
-  end
-
-  # Obtain TMDB data on provided actor 
-  def self.get_tmdb_data(actor)
-    actor_id = self.get_actor_id(actor)
-    self.get_actor_data(actor_id)
-  end
-
-  # Find actors with name that contains input
+  # find actors with a similar name
   def self.search_by_name(name)
     where("name LIKE (?)", "%#{name}%") if name
   end
