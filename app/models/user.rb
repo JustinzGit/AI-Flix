4e092ee2bc5577dedd29e713605b42dadd2db518
Justin <justinms@outlook.com>
2021-05-03 09:37:12 -0500

    remove password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
-  validates :password, presence: true, confirmation: true, length: {in: 8..15}
-  validates :password_confirmation, presence: true
-
   has_secure_password
 end
