d388dbc7246f38d19afdb7a07dca96ada67f6868
Justin <justinms@outlook.com>
2021-05-03 16:03:00 -0500

    add helper to display movies search results

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -9,5 +9,15 @@ module MoviesHelper
 
     def movie_profit(movie)
         number_to_currency(movie[:revenue])
+    end
+    
+    def display_movies_search_results(results)
+        if results
+            count = "<h3 class='list-title'>We found #{results.count} Movies</h3>"
+            movies = results.map do |m|
+                "<p>#{link_to(m.title, movie_path(m))}</p>"
+            end
+            raw(count + movies.join) 
+        end
     end 
 end
