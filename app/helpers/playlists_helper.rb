22548c74ee4c027386f807d4f768e196f219773a
Justin <justinms@outlook.com>
2021-05-04 14:41:42 -0500

    remove link to remove movie

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,10 +1,10 @@
 module PlaylistsHelper
     def display_movies_in_playlist(playlist)
         if playlist.movies.empty?
-            raw("<p>No Movies Have Been Added</p>")
+            raw("<p>No Movies</p>")
         else
             movies = playlist.movies.map do |m|
-                "<p>#{link_to(m.title, movie_path(m), target: :_blank)}&nbsp &nbsp#{link_to('Remove', remove_movie_path(@playlist, m))}</p>"
+                "<p>#{link_to(m.title, movie_path(m), target: :_blank)}</p>"
             end
             raw(movies.join)  
         end
