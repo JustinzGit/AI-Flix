fb0499f2709c56636bb602e14d1abb43c7b8e67b
Justin <justinms@outlook.com>
2021-05-03 21:12:29 -0500

    add helper to display a playlists movies

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,12 @@
 module PlaylistsHelper
+    def display_movies_in_playlist(playlist)
+        if playlist.movies.empty?
+            raw("<p>No Movies Have Been Added</p>")
+        else
+            movies = playlist.movies.map do |m|
+                "<p>#{link_to(m.title, movie_path(m), target: :_blank)}&nbsp &nbsp#{link_to('Remove', remove_movie_path(@playlist, m))}</p>"
+            end
+            raw(movies.join)  
+        end
+    end 
 end
