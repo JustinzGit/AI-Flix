d6633451039e516f670f2539186749edc709ec72
Justin <justinms@outlook.com>
2021-05-04 20:39:44 -0500

    remove unneeded methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,22 +5,7 @@ class ApplicationController < ActionController::Base
 
   def require_login
     if !logged_in?
-      flash[:alert] = "You must be logged in."
       redirect_to login_path
     end
-  end
-
-  def redirect_home_if_logged_in
-    if logged_in?
-      flash[:alert] = "You're already logged in."
-      redirect_to homepage_path
-    end
-  end
-
-  def redirect_if_not_authorized(collection, instance)
-    if !logged_in? || !collection.include?(instance)
-      flash[:alert] = "You're not authorized to access this page"
-      redirect_to homepage_path
-    end 
   end
 end
