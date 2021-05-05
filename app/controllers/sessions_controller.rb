d05ae29399eba8ac4131a80068d693b2c3717e49
Justin <justinms@outlook.com>
2021-05-04 20:43:56 -0500

    remove logic to handle github login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,27 @@
 class SessionsController < ApplicationController
   skip_before_action :require_login
 
+  # GET /login
   def new
     @user = User.new
   end
 
   def create
-    if auth_hash
-      @user = User.find_or_initialize_by(uid: auth_hash[:uid])
-      @user.username = auth_hash[:info][:nickname]
-      @user.save(validate: false)
-      session[:user_id] = @user.id
-      redirect_to homepage_path
-    
-    elsif @user = User.find_by(username: params[:user][:username])
-      if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(email: params[:user][:email])
+
+    # Ensure user is located and password is correct
+    if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to homepage_path
-      end 
     else
-      redirect_to login_path
+        flash[:alert] = "Invalid email or password"
+        redirect_to login_path
     end 
   end
 
+  # GET /logout
   def destroy
     session.clear
     redirect_to login_path
   end
-
-  private
-
-  def auth_hash
-    request.env['omniauth.auth']
-  end 
 end
