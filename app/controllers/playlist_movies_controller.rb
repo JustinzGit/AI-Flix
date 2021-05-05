b16684d68c12e966dca892cc18abc025f53830e9
Justin <justinms@outlook.com>
2021-05-04 20:31:21 -0500

    add create action

--- app/controllers/playlist_movies_controller.rb
+++ app/controllers/playlist_movies_controller.rb
@@ -1,2 +1,14 @@
-class PlaylistMoviesController < ApplicationRecord
+class PlaylistMoviesController < ApplicationController
+
+    # POST /playlist_movies
+    def create
+        @playlist_movie = PlaylistMovie.create(playlist_movie_params)
+        redirect_to playlist_path(@playlist_movie.playlist)
+    end 
+
+    private 
+    
+    def playlist_movie_params
+        params.require(:playlist_movie).permit(:playlist_id, :movie_id)
+    end
 end
