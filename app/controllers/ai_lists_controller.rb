6e53c37d17e1cb8451a20a3d5f3734bbd5a9e095
Justin <justinms@outlook.com>
2021-05-06 15:42:40 -0500

    add logic for finding a movie path

--- app/controllers/ai_lists_controller.rb
+++ app/controllers/ai_lists_controller.rb
@@ -11,7 +11,8 @@ class AiListsController < ApplicationController
 
   # POST /ai_lists
   def create 
-    binding.pry
+    AiList.load_data
+    @movie_path = AiList.find_movie_path(params[:actor_1], params[:actor_2])
   end 
 
   private 
