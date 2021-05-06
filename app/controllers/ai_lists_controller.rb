180cc7225826b9d591eedd954791c0d80b0ae6e5
Justin <justinms@outlook.com>
2021-05-06 14:18:41 -0500

    allow actor params

--- app/controllers/ai_lists_controller.rb
+++ app/controllers/ai_lists_controller.rb
@@ -9,58 +9,14 @@ class AiListsController < ApplicationController
   def new
   end
 
-  # def actor_1
-  #   session[:actor_1] ||= Actor.find(params[:actor_1])
-  #   redirect_to new_user_path_path(current_user)
-  # end
+  # POST /ai_lists
+  def create 
+    binding.pry
+  end 
 
-  # def actor_2
-  #   session[:actor_2] ||= Actor.find(params[:actor_2])
-  #   redirect_to new_user_path_path(current_user)
-  # end
+  private 
 
-  # def clear_actor_names
-  #   session[:actor_1] = nil
-  #   session[:actor_2] = nil
-  #   redirect_to new_user_path_path(current_user)
-  # end
-
-  # def create
-  #   # Acquire actor names from session hash
-  #   actor_1 = Actor.find(session[:actor_1]['id'])
-  #   actor_2 = Actor.find(session[:actor_2]['id'])
-
-  #   # Name path using actor names
-  #   name = "#{actor_1.name} and #{actor_2.name} Path"
-
-  #   # Generate path of movies that link the two actors
-  #   shortest_path = Search.shortest_path(actor_1.id, actor_2.id)
-
-  #   if shortest_path.nil?
-  #     flash[:alert] = "{ Actors Can Not Be Connected }"
-  #     redirect_to new_user_path_path(current_user)
-  #   else
-  #     @path = Path.new(name: name, user_id: session[:user_id])
-
-  #     @path.movies << shortest_path[:movies]
-  #     @path.actors << shortest_path[:actors]
-
-  #     @path.save
-
-  #     redirect_to user_path_path(current_user, @path)
-  #   end
-  # end
-
-  # def show
-  #   @path = Path.find(params[:id])
-  #   redirect_if_not_authorized(current_user.paths, @path)
-  # end
-
-  # def destroy
-  #   path = Path.find(params[:id])
-  #   redirect_if_not_authorized(current_user.paths, path)
-    
-  #   path.destroy
-  #   redirect_to homepage_path
-  # end
+  def ai_list_params
+    params.require(:ai_list).permit(:actor_1, :actor_2)
+  end
 end
