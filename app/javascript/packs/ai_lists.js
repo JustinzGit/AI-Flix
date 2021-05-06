7a84dd50ffe54c860c596b8be47b32a9b207b43b
Justin <justinms@outlook.com>
2021-05-06 09:21:41 -0500

    add button to remove actors

--- app/javascript/packs/ai_lists.js
+++ app/javascript/packs/ai_lists.js
@@ -14,30 +14,48 @@ function addEventsToActors(){
                 myStorage.setItem('actor_1', event.target.id)
                 myStorage.setItem('actor_1_name', event.target.innerText) 
                 displayActors()
+                removeActorsButton()
             }
             else {
                 myStorage.setItem('actor_2', event.target.id)
                 myStorage.setItem('actor_2_name', event.target.innerText)
                 displayActors()
-                displaySubmitButton()
+                removeActorsButton()
+                submitButton()
             }
         })
     })
 }
 
 function displayActors(){
+    actor1Div = document.getElementById('actor_1')
+    actor2Div = document.getElementById('actor_2')
+
     if (myStorage.actor_1 !== undefined){
-        actor1Div = document.getElementById('actor_1')
         actor1Div.innerText = `Actor 1: ${myStorage.actor_1_name}`
     }
     if (myStorage.actor_2 !== undefined){
-        actor2Div = document.getElementById('actor_2')
         actor2Div.innerText = `Actor 2: ${myStorage.actor_2_name}`
     }
 }
 
-function displaySubmitButton(){
+function submitButton(){
     submitButton = document.createElement('button')
     submitButton.innerText = 'Submit Actors'
-    actor2Div.after(submitButton)
+    actorSelections.after(submitButton)
+}
+
+function removeActorsButton(){
+    removeActorsButton = document.createElement('button')
+    removeActorsButton.innerText = 'Remove Actors'
+
+    removeActorsButton.addEventListener('click', () => {
+        myStorage.clear()
+        actor1Div.innerText = `Actor 1: `
+        actor2Div.innerText = `Actor 2: `
+        removeActorsButton.remove()
+    })
+
+    actorSelections = document.getElementById('actor_selections')
+    actorSelections.after(removeActorsButton)
 }
