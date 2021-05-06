ed2f97c8d7deea684ed8b8307a3f67f5aafac5d9
Justin <justinms@outlook.com>
2021-05-06 16:41:38 -0500

    remove unneeded helpers

--- app/helpers/ai_routes_helper.rb
+++ app/helpers/ai_routes_helper.rb
@@ -10,32 +10,7 @@ module AiRoutesHelper
     end
     string.html_safe
   end
-
-  def actor_1_name
-    session[:actor_1]['name'] if session[:actor_1]
-  end
-
-  def actor_2_name
-    session[:actor_2]['name'] if session[:actor_2]
-  end
-
-  def actor_action(actor)
-    if actor_1_name.nil?
-      actor_1_path(actor)
-    else
-      actor_2_path(actor)
-    end
-  end
-
-  def display_users_lists(ai_lists)
-    if ai_lists
-      lists = ai_lists.map do |list|
-        "<p>#{link_to(list.name, ai_list_path(list))}</p>"
-      end
-      raw(lists.join) 
-    end 
-  end
-  
+    
   def display_actor_selection(results)
     if results
         count = "<h3 class='list-title'>We found #{results.count} Actors</h3>"
