00d9094cfe40730b19ab51af39561c991cfb538b
Justin <justinms@outlook.com>
2021-05-06 10:18:20 -0500

    rename buttons, display remove button on dom load

--- app/javascript/packs/ai_lists.js
+++ app/javascript/packs/ai_lists.js
@@ -3,12 +3,17 @@ myStorage = window.localStorage
 document.addEventListener("DOMContentLoaded", () => {
     actor1Div = document.getElementById('actor_1')
     actor2Div = document.getElementById('actor_2')
+    actorSelections = document.getElementById('actor_selections')
 
     // add onclick events to actors in search resutls
     addEventsToActors()
 
     // display actors on page if in local storage
     displayActors()
+
+    if (myStorage.actor_1 !== undefined){
+        removeActorsButton()
+    }
 })
 
 function addEventsToActors(){
@@ -28,23 +33,21 @@ function addEventsToActors(){
                 // display actors on page that have been set
                 displayActors()
 
-                // add button to remove actors
                 removeActorsButton()
+                
             }
 
             // if actor 2 is not in local storage
             else {
                 myStorage.setItem('actor_2', event.target.id)
                 myStorage.setItem('actor_2_name', event.target.innerText)
+                actor2Div.innerText = `Actor 2: ${myStorage.actor_2_name}`
 
                 // display actors on page that have been set
                 displayActors()
 
-                // add button to remove actors
-                removeActorsButton()
-
                 // add button to submit actors
-                submitButton()
+                submitActorsButton()
             }
         })
     })
@@ -55,13 +58,15 @@ function displayActors(){
     if (myStorage.actor_1 !== undefined){
         actor1Div.innerText = `Actor 1: ${myStorage.actor_1_name}`
     }
+
     if (myStorage.actor_2 !== undefined){
         actor2Div.innerText = `Actor 2: ${myStorage.actor_2_name}`
     }
+
 }
 
 // button to submit actors to generate ai movie list
-function submitButton(){
+function submitActorsButton(){
     submitButton = document.createElement('button')
     submitButton.innerText = 'Submit Actors'
     actorSelections.after(submitButton)
@@ -69,16 +74,15 @@ function submitButton(){
 
 // button to remove actors from DOM and clear local storage
 function removeActorsButton(){
-    removeActorsButton = document.createElement('button')
-    removeActorsButton.innerText = 'Remove Actors'
+    removeButton = document.createElement('button')
+    removeButton.innerText = 'Remove Actors'
 
-    removeActorsButton.addEventListener('click', () => {
+    removeButton.addEventListener('click', () => {
         myStorage.clear()
         actor1Div.innerText = `Actor 1: `
         actor2Div.innerText = `Actor 2: `
-        removeActorsButton.remove()
+        removeButton.remove()
     })
 
-    actorSelections = document.getElementById('actor_selections')
-    actorSelections.after(removeActorsButton)
+    actorSelections.after(removeButton)
 }
