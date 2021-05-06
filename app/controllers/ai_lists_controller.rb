5d456bc7a3325f87abd558123f1ef1d21255c24c
Justin <justinms@outlook.com>
2021-05-06 15:49:06 -0500

    remove whitelisting of params

--- app/controllers/ai_lists_controller.rb
+++ app/controllers/ai_lists_controller.rb
@@ -9,10 +9,4 @@ class AiListsController < ApplicationController
     AiList.load_data
     @movie_path = AiList.find_movie_path(params[:actor_1], params[:actor_2])
   end 
-
-  private 
-
-  def ai_list_params
-    params.require(:ai_list).permit(:actor_1, :actor_2)
-  end
 end
