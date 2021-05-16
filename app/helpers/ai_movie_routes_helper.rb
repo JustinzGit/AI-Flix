93d35b35ff5190d688f26103794dea35d8e31d4c
Justin <justinms@outlook.com>
2021-05-16 07:44:42 -0500

    modify method to display movie path

--- app/helpers/ai_movie_routes_helper.rb
+++ app/helpers/ai_movie_routes_helper.rb
@@ -1,19 +1,81 @@
 module AiMovieRoutesHelper
 
-  def display_movie_path(movie_path)
+  def display_movie_path(path)
+    html = []
+    movie_count = path[:movies].length
 
+    movie_count.times.map do |index|
+      index % 2 == 0 ? orientation = "right" : orientation = "left"
+      html << """
+        <div class='actors-section connection-#{orientation}'>
+          <div class='actor-right w-container'>
 
-    movies = movie_path[:movies].map {|m| m.title}
-    actors = movie_path[:actors].map {|a| a.name}
-    path = []
+              #{
+                if orientation == "right"
+                  "<div class='actor-wrapper'>
+                   <h3 class='title-heading'>#{path[:actors][index][:name]}</h3>
+                   #{image_tag("https://image.tmdb.org/t/p/original#{path[:actors][index][:image]}", class: "show-image small")}"
+                else 
+                  "<div class='actor-wrapper left with-connection'>
+                   <h3 class='title-heading'>#{path[:actors][index + 1][:name]}</h3>
+                   #{image_tag("https://image.tmdb.org/t/p/original#{path[:actors][index + 1][:image]}", class: "show-image small")}"
+                end 
+              }
+            
+              #{
+                if orientation == "left"
+                  "<div class='line-div' #{"style='visibility: hidden'" if index + 1 == movie_count} ><div class='line vertical right'></div></div>"
+                end 
+              }
+              
+            </div>
 
+            <div class='circle'></div>
+          </div>
 
+          <div class='circle single'></div>
 
-    movies.length.times do |i|
-      path << "<p>#{actors[i]} and #{actors[i + 1]} starred in #{movies[i]}</p>"
+          <div class='path-connector'>
+            <h3 class='title-heading'>Starred In</h3>
+            <div class='line horizontal'></div>
+            <div class='line vertical'></div>
+          </div>
+
+          <div class='actor-left w-container'>
+            <div class='circle'></div>
+
+            #{
+              if orientation == "right"
+                "<div class='actor-wrapper right with-connection'>
+                 <h3 class='title-heading'>#{path[:actors][index + 1][:name]}</h3>
+                 #{image_tag("https://image.tmdb.org/t/p/original#{path[:actors][index + 1][:image]}", class: "show-image small")}"
+              else 
+                "<div class='actor-wrapper'>
+                <h3 class='title-heading'>#{path[:actors][index][:name]}</h3>
+                #{image_tag("https://image.tmdb.org/t/p/original#{path[:actors][index][:image]}", class: "show-image small")}"
+              end 
+              }
+              #{
+                if orientation == "right"
+                  "<div class='line-div' #{"style='visibility: hidden'" if index + 1 == movie_count} ><div class='line vertical right'></div></div>"
+                end 
+              }
+            </div>
+          </div>
+        </div>
+
+        <h1 class='white-heading small'>Starred In </h1>
+        <div class='movies-section'>
+          <div class='movie-wrapper'>
+            <div class='circle margin-bottom'></div>
+            <h3 class='title-heading'>#{path[:movies][index][:title]}</h3>
+            #{image_tag("https://image.tmdb.org/t/p/original#{path[:movies][index][:poster_path]}", class: "show-image small")}
+          </div>
+        </div>
+      """
     end
-    raw(path.join)
-  end
+    raw(html.join) 
+  end 
 
   def display_actor_search_results(results)
     if results
