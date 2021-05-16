9a5e199ab46dfd95600475413e9424ce574de20e
Justin <justinms@outlook.com>
2021-05-15 19:01:44 -0500

    modify styling

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,10 +1,10 @@
 module PlaylistsHelper
     def display_movies_in_playlist(playlist)
         if playlist.movies.empty?
-            raw("<p>No Movies</p>")
+            raw("<h2 class='white-heading'>No Movies Added</h2>")
         else
             movies = playlist.movies.map do |m|
-                "<p>#{link_to(m.title, movie_path(m), target: :_blank)}</p>"
+                "#{link_to(m.title, movie_path(m), target: :_blank, class: "grey-link")}"
             end
             raw(movies.join)  
         end
