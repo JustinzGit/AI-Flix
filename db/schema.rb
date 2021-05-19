0f01160d8e63e9ed5ec23eeae43ad4fd699cca09
Justin <justinms@outlook.com>
2021-05-19 13:59:06 -0500

    enable extension for pgsql db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_09_150309) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "actors", force: :cascade do |t|
     t.integer "tmdb_id"
     t.string "name"
