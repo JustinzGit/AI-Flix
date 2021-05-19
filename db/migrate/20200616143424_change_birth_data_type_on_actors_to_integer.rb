ad335baca51372eb4b9a126f07d105e69669ce44
Justin <justinms@outlook.com>
2021-05-19 11:59:30 -0500

    confirm integer usage

--- db/migrate/20200616143424_change_birth_data_type_on_actors_to_integer.rb
+++ db/migrate/20200616143424_change_birth_data_type_on_actors_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeBirthDataTypeOnActorsToInteger < ActiveRecord::Migration[6.0]
   def change
-    change_column :actors, :birth, :integer
+    change_column :actors, :birth, 'integer USING CAST(birth AS integer)'
   end
 end
