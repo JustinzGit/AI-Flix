0e396cb6a8da1c2606a22cc50761b377785a61ba
Justin <justinms@outlook.com>
2021-05-19 12:15:49 -0500

    specify integer type

--- db/migrate/20200616143653_change_year_data_type_on_movies_to_integer.rb
+++ db/migrate/20200616143653_change_year_data_type_on_movies_to_integer.rb
@@ -1,5 +1,5 @@
 class ChangeYearDataTypeOnMoviesToInteger < ActiveRecord::Migration[6.0]
   def change
-    change_column :movies, :year, :integer
+    change_column :movies, :year, 'integer USING CAST(birth AS integer)'
   end
 end
