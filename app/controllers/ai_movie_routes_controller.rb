b3761592826ed47a816d1a0adeb9f7afef11224e
Justin <justinms@outlook.com>
2021-05-26 11:46:51 -0500

    inform user that data is still being loaded

--- app/controllers/ai_movie_routes_controller.rb
+++ app/controllers/ai_movie_routes_controller.rb
@@ -6,6 +6,11 @@ class AiMovieRoutesController < ApplicationController
 
   # GET /ai_movie_routes/generate
   def generate
-    @movie_path = Search.generate_movie_path(params[:actor_1], params[:actor_2])
+    if !Search.load_complete?
+      flash[:alert] = "Still Loading Data. Try again in a few seconds"
+      render :new
+    else 
+      @movie_path = Search.generate_movie_path(params[:actor_1], params[:actor_2])
+    end 
   end 
 end
